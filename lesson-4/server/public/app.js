e735b808fc95302e73b2b75779e56b58704b60d0
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-07-24 02:59:03 +0100

    fixed some typeError in form-msg

--- lesson-4/server/public/app.js
+++ lesson-4/server/public/app.js
@@ -54,7 +54,7 @@ socket.on("message", (data) => {
     <span class="post__header--name">${name}</span>
     <span class="post__header--time">${time}</span>
   </div>
-  <div clas="post__text">${text}</div>`;
+  <div class="post__text">${text}</div>`;
   } else {
     li.innerHTML = `<div class="post__text">${text}</div>`;
   }
